f5ab86cac0df0719d68c8a7e53043d607ea4a1e3
lavachemist <thelavachemist@gmail.com>
2020-06-06 13:47:22 -0400

    Update GCodeWriter.cpp

--- src/libslic3r/GCodeWriter.cpp
+++ src/libslic3r/GCodeWriter.cpp
@@ -443,7 +443,7 @@ bool GCodeWriter::will_move_z(double z) const
 long slapoint (fdmpoint)
 {
     int laserpoint = std::llround(fdmpoint * 524.288);
-    return laserpoint();
+    return laserpoint;
 }
 
 std::string GCodeWriter::extrude_to_xy(const Vec2d &point, double dE, const std::string &comment)
