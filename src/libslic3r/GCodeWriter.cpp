545b752883c7e07775981eb8826f65704c70ee67
lavachemist <thelavachemist@gmail.com>
2020-06-06 16:45:07 -0400

    Update GCodeWriter.cpp

--- src/libslic3r/GCodeWriter.cpp
+++ src/libslic3r/GCodeWriter.cpp
@@ -454,7 +454,7 @@ std::string GCodeWriter::extrude_to_xy(const Vec2d &point, double dE, const std:
         gcode << "LaserPoint(";
         gcode << "x=" << round(point.x() * 524.88);
         gcode << ", y=" << (point.y() * 524.88);
-        gcode << ", dt=" << (m_tool->E() * 1000);
+        gcode << ", dt=" << round(m_tool->E() * 1000);
         gcode << ")\n";
         return gcode.str();
     } else {
