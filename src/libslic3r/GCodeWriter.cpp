7c551796b304fea9abb18165cfd70c487b3768db
lavachemist <thelavachemist@gmail.com>
2020-06-06 12:45:57 -0400

    Update GCodeWriter.cpp

--- src/libslic3r/GCodeWriter.cpp
+++ src/libslic3r/GCodeWriter.cpp
@@ -448,16 +448,12 @@ std::string GCodeWriter::extrude_to_xy(const Vec2d &point, double dE, const std:
         bool is_extrude = m_tool->extrude(dE) != 0;
         
         std::ostringstream gcode;
-        gcode << "0x00 XY Move \n";
+        gcode << "0x00 XY Move 1\n";
         gcode << "LaserPoint(";
         gcode << "x=" << XYZF_NUM(point.x());
         gcode << ", y=" << XYZF_NUM(point.y());
         gcode << ", dt=" << "666";
-        gcode << ")";
-        if(is_extrude)
-            gcode <<    " " << m_extrusion_axis << E_NUM(m_tool->E());
-        COMMENT(comment);
-        gcode << "\n";
+        gcode << ")\n";
         return gcode.str();
     } else {
         m_pos.x() = point.x();
