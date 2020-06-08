6c58d8084f3773b148fb225b084ff7711fe4bfbc
lavachemist <thelavachemist@gmail.com>
2020-06-08 13:32:07 -0400

    Update GCodeWriter.cpp

--- src/libslic3r/GCodeWriter.cpp
+++ src/libslic3r/GCodeWriter.cpp
@@ -450,10 +450,10 @@ std::string GCodeWriter::_travel_to_z(double z, const std::string &comment)
         m_pos.z() = z;
         
         std::ostringstream gcode;
-        gcode << "0x04 ZFeedRate " << z_travel_openfl * 400;
+        gcode << "0x04 ZFeedRate " << XYZF_NUM(this->config.travel_speed.value);
         gcode << "\n";
         gcode << "0x03 ZMove ";
-        gcode << XYZF_NUM(z);
+        gcode << z_travel_openfl * 400;
         gcode << "\n";
         return gcode.str();
     } else {
