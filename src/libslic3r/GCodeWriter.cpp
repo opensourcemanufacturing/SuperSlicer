ab0f60e04dfdf5398689c8cf54d020cdbf91f774
lavachemist <thelavachemist@gmail.com>
2020-06-08 09:35:08 -0400

    Update GCodeWriter.cpp

--- src/libslic3r/GCodeWriter.cpp
+++ src/libslic3r/GCodeWriter.cpp
@@ -314,7 +314,7 @@ std::string GCodeWriter::toolchange(unsigned int tool_id)
 std::string GCodeWriter::set_speed(double F, const std::string &comment, const std::string &cooling_marker) const
 {        
     std::ostringstream gcode;
-    m_last_speed = (60000/F);
+    m_last_speed = (1000/F);
 
     if (FLAVOR_IS(gcfopenfl)){
         assert(F > 0.);
