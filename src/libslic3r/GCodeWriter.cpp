66555eb0a018e9879c367b51db7ed610cfd1f5ae
lavachemist <thelavachemist@gmail.com>
2020-06-09 10:52:13 -0400

    Update GCodeWriter.cpp

--- src/libslic3r/GCodeWriter.cpp
+++ src/libslic3r/GCodeWriter.cpp
@@ -468,8 +468,8 @@ std::string GCodeWriter::_travel_to_z(double z, const std::string &comment)
     if(FLAVOR_IS(gcfopenfl)){ 
         // declare variables
         int m_z_move; // layer height variable
-        float m_last_z = m_pos.z(); // hold the value of the last Z move
-        m_pos.z() = z; // value of next Z move (in microsteps)
+        double m_last_z = m_pos.z(); // hold the value of the last Z move
+        m_pos.z() = z; // value of next Z move in mm
         
 
         if (m_last_z > 0){ // If this is not the first layer do this:
