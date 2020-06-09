2bb0843c724c110c8dc2b52dd30366c6a8a48bfe
lavachemist <thelavachemist@gmail.com>
2020-06-09 11:27:24 -0400

    Update GCodeWriter.cpp

--- src/libslic3r/GCodeWriter.cpp
+++ src/libslic3r/GCodeWriter.cpp
@@ -466,6 +466,7 @@ std::string GCodeWriter::_travel_to_z(double z, const std::string &comment)
     */
 
     if(FLAVOR_IS(gcfopenfl)){ 
+        std::ostringstream gcode;
         // declare variables
         int m_z_move; // layer height variable
         float m_last_z = m_pos.z(); // hold the value of the last Z move
@@ -474,7 +475,6 @@ std::string GCodeWriter::_travel_to_z(double z, const std::string &comment)
 
         if (m_last_z > 0.000){ // If this is not the first layer do this:
             m_z_move = (m_pos.z() - m_last_z) * 400; // layer height = next z move minus last z move
-            std::ostringstream gcode;
             gcode << "0x04 ZFeedRate " << XYZF_NUM(this->config.travel_speed.value); // FLP feed rate command
             gcode << "\n";
             gcode << "0x03 ZMove 2000"; // 5mm peel lift (in microsteps)
@@ -485,7 +485,6 @@ std::string GCodeWriter::_travel_to_z(double z, const std::string &comment)
             return gcode.str();
         } else { // otherwise do this, because it is a first layer
             m_z_move = m_pos.z() * 400; 
-            std::ostringstream gcode;
             gcode << "0x04 ZFeedRate " << XYZF_NUM(this->config.travel_speed.value); // FLP feed rate command
             gcode << "\n";
             gcode << "0x03 ZMove ";
