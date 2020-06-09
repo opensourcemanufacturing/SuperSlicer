658d6b16c2056d5e4bad91fb45b39a032591fe37
lavachemist <thelavachemist@gmail.com>
2020-06-09 14:41:19 -0400

    Update GCodeWriter.cpp

--- src/libslic3r/GCodeWriter.cpp
+++ src/libslic3r/GCodeWriter.cpp
@@ -474,13 +474,13 @@ std::string GCodeWriter::_travel_to_z(double z, const std::string &comment)
         
 
         if (m_last_z > 0.){ // If this is not the first layer do this:
-            m_z_move = int((m_pos.z() * 400) - int(m_last_z) * 400).; // layer height = next z move minus last z move times 400 microsteps
+            m_z_move = int((m_pos.z() * 400) - int(m_last_z) * 400); // layer height = next z move minus last z move times 400 microsteps
             gcode << "0x04 ZFeedRate " << XYZF_NUM(this->config.travel_speed.value); // FLP feed rate command
             gcode << "\n";
             gcode << "0x03 ZMove 2000"; // 5mm peel lift (in microsteps)
             gcode << "\n";
             gcode << "0x03 ZMove ";
-            gcode << m_z_move - 2000.; // unpeel and reset for next layer (in microsteps)
+            gcode << m_z_move - 2000; // unpeel and reset for next layer (in microsteps)
             gcode << "\n";
             return gcode.str();
         } else { // otherwise do this, because this is the first layer:
