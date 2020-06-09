22b706f23f935c55360b19c9e0cc3a46a3d99e30
lavachemist <thelavachemist@gmail.com>
2020-06-09 10:43:44 -0400

    Update GCodeWriter.cpp

--- src/libslic3r/GCodeWriter.cpp
+++ src/libslic3r/GCodeWriter.cpp
@@ -480,7 +480,7 @@ std::string GCodeWriter::_travel_to_z(double z, const std::string &comment)
             gcode << "0x03 ZMove 2000"; // 5mm peel lift (in microsteps)
             gcode << "\n";
             gcode << "0x03 ZMove ";
-            gcode << int m_z_move - 2000; // unpeel and set for next layer
+            gcode << int(m_z_move) - 2000; // unpeel and set for next layer
             gcode << "\n";
             return gcode.str();
         } else { // otherwise do this, because it is a first layer
@@ -489,7 +489,7 @@ std::string GCodeWriter::_travel_to_z(double z, const std::string &comment)
             gcode << "0x04 ZFeedRate " << XYZF_NUM(this->config.travel_speed.value); // FLP feed rate command
             gcode << "\n";
             gcode << "0x03 ZMove ";
-            gcode << int m_z_move; // first layer height in microsteps
+            gcode << int(m_z_move); // first layer height in microsteps
             gcode << "\n";
             return gcode.str();
         }
