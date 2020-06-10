2e0352792387e82b176bf23fcd5c0983a078cbae
lavachemist <thelavachemist@gmail.com>
2020-06-10 09:53:22 -0400

    Update GCodeWriter.cpp

--- src/libslic3r/GCodeWriter.cpp
+++ src/libslic3r/GCodeWriter.cpp
@@ -518,7 +518,8 @@ std::string GCodeWriter::_travel_to_z(double z, const std::string &comment)
 
         if (m_last_z > 0.){ // If this is not the first layer do this:
             m_z_move_d = ((m_pos.z() - m_last_z) * 400) * 100; // layer height = next z move minus last z move times 400 microsteps
-            m_z_move = floor((m_z_move_d) / 100);
+            m_z_move_i = floor(m_z_move_d);
+            m_z_move = m_z_move_i / 100;
             gcode << "0x04 ZFeedRate " << XYZF_NUM(this->config.travel_speed.value); // FLP feed rate command
             gcode << "\n";
             gcode << "0x03 ZMove 2000"; // 5mm peel lift (in microsteps)
@@ -527,18 +528,23 @@ std::string GCodeWriter::_travel_to_z(double z, const std::string &comment)
             gcode << m_z_move; // unpeel and reset for next layer (in microsteps)
             printf("%11.6f ", m_last_z);
             printf("%11.6f ", m_pos.z());
+            printf("%d ", m_z_move_d);
+            printf("%d ", m_z_move_i);
             printf("%d\n ", m_z_move);
             gcode << "\n";
             return gcode.str();
         } else { // otherwise do this, because this is the first layer:
             m_z_move_d = (m_pos.z() * 400) * 100;
-            m_z_move = floor(m_z_move_d) / 100;
+            m_z_move_i = floor(m_z_move_d);
+            m_z_move = m_z_move_i / 100;
             gcode << "0x04 ZFeedRate " << XYZF_NUM(this->config.travel_speed.value); // FLP feed rate command
             gcode << "\n";
             gcode << "0x03 ZMove ";
             gcode << m_z_move; // first layer height in microsteps
             printf("%11.6f ", m_last_z);
             printf("%11.6f ", m_pos.z());
+            printf("%d ", m_z_move_d);
+            printf("%d ", m_z_move_i);
             printf("%d\n ", m_z_move);
             gcode << "\n";
             return gcode.str();
